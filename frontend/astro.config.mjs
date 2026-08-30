e9df47d2012ac3cd9118b9fc8fcca2c955f61577
MonroeHumane <jeffhoward@monroe-humane.org>
2026-08-30 18:46:42 -0400

    4

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://monroe-humane.org',
   output: 'static', 
-  adapter: node({ mode: 'standalone' }),
   integrations: [sitemap()],
 });
